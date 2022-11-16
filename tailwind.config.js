01b6980cf7f6a6bcaaf2a37767b26ac631e6289f
asepsurahman03 <117372862+asepsurahman03@users.noreply.github.com>
2022-11-16 20:07:35 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#42A66A',
+                secondary: '#ff3b07',
+                nav: '#969696',
+                header: '#F6F9FC',
+            },
+            fontFamily: {
+                raleway: ['Raleway', 'sans-serif'],
+                nunito: ['Nunito Sans', 'sans-serif'],
+            }
+        },
+    },
+}
